50260e3dce95b6fff344b3cbc035d70df336cc19
Serhii Veretennikov <gridis72@gmail.com>
2025-05-02 16:03:09 +0200

    refactor: changed copy constructor from copying address to proper copying of value

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: svereten <svereten@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 15:57:00 by svereten          #+#    #+#             */
-/*   Updated: 2025/05/01 19:30:39 by svereten         ###   ########.fr       */
+/*   Updated: 2025/05/02 16:02:58 by svereten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "Fixed.hpp"
@@ -17,10 +17,8 @@ Fixed::Fixed(): _value(0) {
 	std::cout << "Default constructor called\n";
 }
 
-Fixed::Fixed(const Fixed &fixed) {
+Fixed::Fixed(const Fixed &fixed): _value(fixed._value) {
 	std::cout << "Copy constructor called\n";
-
-	*this = fixed;
 }
 
 Fixed::Fixed(const int value) {
